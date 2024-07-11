8ce9ef97d57a87c6fc4f4eb9895cc430e4e103b8
Le Tuan Dan <letuandan_t66@hus.edu.vn>
2024-07-11 13:48:59 +0700

    update reset and forgot

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <LoginPage/>
+  <ResetPassword/>
+  <ForgotPassword/>
 </template>
 
 <script>
 import LoginPage from './components/auth/Login_page.vue'
+import ResetPassword from './components/auth/Reset_password.vue'
+import ForgotPassword from './components/auth/Forgot_password.vue'
 
 export default {
   name: 'App',
   components: {
-    LoginPage
+    LoginPage,
+    ResetPassword,
+    ForgotPassword
   }
 }
 </script>
